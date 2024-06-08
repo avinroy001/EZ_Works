5859e630e6fb0b380ac21e1cb4f2107e123105b8
Avinash <avin.roy001@gmail.com>
2024-06-08 14:16:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import logo from "./assets/EZlogo.png"
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="leftWrapper">
+      <div>
+        <img src={logo} alt="logo" width={300}/>
+        <p>Suite Of Business Support Services</p>
+        <div>
+       
+        </div>
+      </div>
     </div>
   );
 }
